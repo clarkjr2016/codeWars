6343942dd5946e301d5f02ea898e2287ef111d09
Eugene Clark <clarkjr2016@gmail.com>
2024-09-06 04:21:59 -0400

    boolToWord completed

---
+++ boolToWord/boolToWord.js
@@ -0,0 +1,16 @@
+function boolToWord(bool) {
+  let returnVal;
+
+  if (bool === true) {
+    returnVal = "Yes";
+  } else if (bool === false) {
+    returnVal = "No";
+  }
+
+  return returnVal;
+}
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+//I start with a boolean value being entered into the function
+
+//I return a "Yes" value if boolean equals true and a "No" value if value is false
--- postiveSum/positiveSum.js
+++ postiveSum/positiveSum.js
@@ -1,4 +1,10 @@
 function positiveSum(arr) {
+  //Need a variable to return the sum with
+
+  //Need a way to decide if an array is empty or not
+
+  //Need a way to loop through each element and decide if it's a positive number
+
   let sum = 0;
 
   if (arr.length === 0) {
