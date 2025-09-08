9b587d60303357d1fdd8ffaac526aa379b64a72b
Eugene Clark <clarkjr2016@gmail.com>
2025-09-07 21:35:56 -0400

    My solution was the same as the top one of the other solutions!

---
+++ testing123/testing123.js
@@ -0,0 +1,28 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+var number = function (array) {
+  //Parameters = the array that's being inputed into the function
+
+  //return value = that array prepended with the appropriate number for the string
+
+  //example = ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+  //psuedocode =
+
+  let prependedArray = array.map((string, index) => {
+    return `${index + 1}: ${string}`;
+  });
+
+  return prependedArray;
+};
+
+console.log(number(["a", "b", "c"]));
